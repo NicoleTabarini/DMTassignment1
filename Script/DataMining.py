fb265bd317a4b8fa870db34ab9d773ead145854b
NicoleTabarini <nicole.tabarini98@gmail.com>
2022-04-16 19:02:31 +0200

    Pre-processing + windowing

--- Script/DataMining.py
+++ Script/DataMining.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from datetime import datetime
 
-
+# Rearrange original dataset to have each day as a row and all variable as columns
 data = pd.read_csv("dataset_mood_smartphone.csv")
 d = data.iloc[:, 1:]
 d[['date','time']] = d.time.str.split(" ",expand=True)
@@ -19,6 +20,7 @@ mod = d.groupby(["id","date","variable"],as_index=False)["value"].mean()
 new=pd.DataFrame(mod["id"],columns=["id","date",'mood', 'circumplex.arousal', 'circumplex.valence', 'activity', 'screen', 'call', 'sms', 'appCat.builtin', 'appCat.communication', 'appCat.entertainment', 'appCat.finance', 'appCat.game', 'appCat.office', 'appCat.other', 'appCat.social', 'appCat.travel', 'appCat.unknown', 'appCat.utilities', 'appCat.weather'])
 new["date"] = mod["date"]
 new_group = new.groupby(["id","date"],as_index=False).mean()
+#print(new_group)
 
 ind = 0
 for row in mod.itertuples():
@@ -29,5 +31,125 @@ for row in mod.itertuples():
         if mod["date"].iloc[row.Index+1] != mod["date"].iloc[row.Index]:
                 ind += 1
 
-new_group.to_csv("new.csv",na_rep='NA')
-print(new_group)
+#new_group.to_csv("new.csv",na_rep='NA')
+
+
+
+
+# Obtain the same as new_group, but with the sum for some variables instead of mean
+data_sum = pd.read_csv("mood_data_sum.csv")
+rearranged  = pd.DataFrame(new_group["id"],columns=["id","date","mean_mood", 'mean_circumplex.arousal', 'mean_circumplex.valence', 'sum_call', 'sum_sms', 'mean_activity', 'sum_screen', 'sum_appCat.builtin', 'sum_appCat.communication', 'sum_appCat.entertainment', 'sum_appCat.finance', 'sum_appCat.game', 'sum_appCat.office', 'sum_appCat.other', 'sum_appCat.social', 'sum_appCat.travel', 'sum_appCat.unknown', 'sum_appCat.utilities', 'sum_appCat.weather'])
+rearranged["date"]=new_group["date"]
+
+v_names=list(data_sum.columns.values)
+for n in range(2,len(v_names)):
+    rearranged[v_names[n]]=data_sum[v_names[n]]
+
+#rearranged.to_csv("rearranged.csv",na_rep='NA')
+
+
+
+
+
+# Remove empty rows
+no_missing = rearranged[["mean_mood", 'mean_circumplex.arousal', 'mean_circumplex.valence', 'mean_activity', 'sum_screen', 'sum_appCat.builtin', 'sum_appCat.communication', 'sum_appCat.entertainment', 'sum_appCat.finance', 'sum_appCat.game', 'sum_appCat.office', 'sum_appCat.other', 'sum_appCat.social', 'sum_appCat.travel', 'sum_appCat.unknown', 'sum_appCat.utilities', 'sum_appCat.weather']].isna().all(axis=1)
+for row in range(len(no_missing)):
+    if no_missing[row]:
+        try:
+            cleaned = cleaned.drop(index=row)
+        except:
+            cleaned = rearranged.drop(index=row)
+#print(cleaned)
+#cleaned.to_csv("cleaned.csv",na_rep='NA')
+
+
+
+
+# Remove not consecutive days
+patients = [y for x, y in cleaned.groupby('id', as_index=False)]
+
+
+for p in patients:
+    p.reset_index(inplace=True,drop=True)
+    dates = [datetime.strptime(d, "%Y-%m-%d") for d in p["date"]]
+    date_ints = pd.DataFrame([d.toordinal() for d in dates])
+    for el in range(len(date_ints)-5):
+        if int(date_ints.iloc[el+5,]) != int(date_ints.iloc[el,]+5):
+            try:
+                f_clean = f_clean.drop(index=el)
+            except:
+                f_clean = p.drop(index=el)
+
+        if int(date_ints.iloc[el+5,]) == int(date_ints.iloc[el,]+5) and el == len(date_ints)-6:
+            try:
+                f_clean
+            except:
+                f_clean=p
+    #print(f_clean)
+    try:
+        final = final.append(f_clean)
+    except:
+        final = f_clean
+    del f_clean
+
+#final.to_csv("final_withNA.csv",na_rep='NA')
+
+
+
+
+
+# Insert mean value per patient whenever the mood value is missing
+mean_mood=[]
+for p in patients:
+    mean_mood.append(p["mean_mood"].mean())
+
+patients = [y for x, y in final.groupby('id', as_index=False)]
+
+final.reset_index(inplace=True,drop=True)
+i=0
+f_row=0
+f_final = final
+for p in patients:
+    for row in p.itertuples():
+        if p.isnull().loc[row.Index,"mean_mood"]:
+            f_final.at[f_row,"mean_mood"] = mean_mood[i]
+        f_row += 1
+    i+=1
+
+#f_final.to_csv("FINAL_FINAL.csv",na_rep='NA')
+
+
+
+
+
+# Windowing: average together N days
+STEP = 5
+patients = [y for x, y in f_final.groupby('id', as_index=False)]
+
+def computeavg(data):
+    data.reset_index(inplace=True, drop=True)
+    avg = pd.DataFrame(data["id"],
+                       columns=["id","date","mean_mood", 'mean_circumplex.arousal', 'mean_circumplex.valence',
+                                'sum_call', 'sum_sms', 'mean_activity', 'sum_screen', 'sum_appCat.builtin',
+                                'sum_appCat.communication', 'sum_appCat.entertainment', 'sum_appCat.finance',
+                                'sum_appCat.game', 'sum_appCat.office', 'sum_appCat.other', 'sum_appCat.social',
+                                'sum_appCat.travel', 'sum_appCat.unknown', 'sum_appCat.utilities',
+                                'sum_appCat.weather',"TARGET"])
+    for row in range(len(data) - STEP):
+        for col in range(2, len(data.columns)):
+            avg.iat[row, col] = data.iloc[row:row + STEP, col].mean()
+    for row in avg.itertuples():
+        if row.Index < len(avg)-5:
+            avg.at[row.Index,"TARGET"] = data.loc[row.Index+STEP,"mean_mood"]
+    return avg
+
+
+for i in patients:
+    try:
+        avg_merged = avg_merged.append(computeavg(i))
+    except:
+        avg_merged = computeavg(i)
+
+# remove the last 5 empty rows for each patient
+avg5_final = avg_merged.dropna(thresh=2)
+avg5_final.to_csv("5days_final_final.csv")
